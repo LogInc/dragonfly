567ef5014d826d96d85983c2006deac6fb7060c1
Abdullah Baig <abdullahbaig456@gmail.com>
2016-03-29 20:01:44 +0500

    Dragonfly: ImageBox control showing image

--- dragonfly/DroneVision.cs
+++ dragonfly/DroneVision.cs
@@ -24,15 +24,36 @@ namespace Dragonfly
     /// </summary>
     public static class DroneVision
     {
+        /// <summary>
+        /// Used to specify which device to select for capturing images.
+        /// </summary>
+        public enum VideoSource
+        {
+            Drone,
+            Webcam,
+        }
+
         private static Capture capture;
         private static DispatcherTimer timer_job;
+        private static Mat captured_frame = new Mat();
+        private static Mat composed_image = new Mat();
 
-        public static void Initialize()
+        public static void Initialize(VideoSource source=VideoSource.Webcam)
         {
             capture = new Capture();
             timer_job = new DispatcherTimer();
             timer_job.Tick += new EventHandler(Job_Tick);
-            timer_job.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            timer_job.Interval = new TimeSpan(0, 0, 0, 0, 1); // 1 ms
+        }
+
+        public static Emgu.CV.Image<Bgr, Byte> GetImage()
+        {
+            if (composed_image.Bitmap != null)
+            {
+                var image = composed_image.ToImage<Emgu.CV.Structure.Bgr, Byte>();
+                return image;
+            }
+            return null;
         }
 
         public static void Start()
@@ -47,25 +68,30 @@ namespace Dragonfly
 
         private static void Job_Tick(object sender, EventArgs e)
         {
-            Mat frame = new Mat();
-            capture.Retrieve(frame);
-
             Mat gray = new Mat();
-            CvInvoke.CvtColor(frame, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+            Mat temp = new Mat();
+
+            capture.Retrieve(captured_frame);
+            composed_image = captured_frame;
+            // Color to gray conversion
+            CvInvoke.CvtColor(captured_frame, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+
+            // Histogram equalization
             CvInvoke.EqualizeHist(gray, gray);
 
-            Mat temp = new Mat();
+            // Noise removal
             CvInvoke.GaussianBlur(gray, temp, new Size(3, 3), 1);
             gray = temp;
 
             List<Rectangle> detectedFaces = new List<Rectangle>();
             FaceDetector.DetectFace(gray, detectedFaces);
+            // Draw rectangles around each face
             foreach (var face in detectedFaces)
             {
-                CvInvoke.Rectangle(gray, face, new MCvScalar(255, 0, 0));
+                CvInvoke.Rectangle(temp, face, new MCvScalar(255, 0, 0));
             }
 
-            CvInvoke.Imshow("Gray", gray);
+            CvInvoke.Imshow("Faces", temp);
         }
     }
 }
--- dragonfly/MainWindow.xaml.cs
+++ dragonfly/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Emgu.CV;
+using System.Runtime.InteropServices;
 
 namespace Dragonfly
 {
@@ -27,11 +28,55 @@ namespace Dragonfly
             InitializeComponent();
         }
 
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Converts an Emgu CV image object to a BitmapSource object that can
+        /// be used as a source to a WPF control.
+        /// </summary>
+        /// <param name="image">The image to convert</param>
+        /// <returns>Convert image</returns>
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap();
+
+                BitmapSource bs = System.Windows.Interop
+                  .Imaging.CreateBitmapSourceFromHBitmap(
+                  ptr,
+                  IntPtr.Zero,
+                  Int32Rect.Empty,
+                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
+        }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             DroneVision.Initialize();
             DroneVision.Start();
+
+            Task imageUpdateTask = Task.Run(() => UpdateImageTask());
+        }
+
+        private void UpdateImageTask()
+        {
+            while (true)
+            {
+                IImage image = DroneVision.GetImage();
+                if (image != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        ImageSource source = ToBitmapSource(image);
+                        image_droneview.Source = source;
+                    }));
+                }
+            }
         }
     }
 }
